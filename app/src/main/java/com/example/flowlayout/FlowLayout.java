962f660c58f0f1b46dcd7141817da5052387ac09
zhouss <zz_shanshan@126.com>
2019-08-27 12:30:24 +0800

    all

---
+++ app/src/main/java/com/example/flowlayout/FlowLayout.java
@@ -0,0 +1,148 @@
+package com.example.flowlayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlowLayout extends ViewGroup {
+
+    //上下左右padding
+    private final int PADDING = dp2px(15);
+    //子View的间隔
+    private final int ITEM_INVTERVAL = dp2px(10);
+    //行间隔；
+    private final int LINE_INTERVAL = dp2px(20);
+
+    //记录每一行的最大高度
+    private List<Integer> maxHeights = new ArrayList<>();
+
+    //记录每一行的View集合
+    private List<List<View>> lines = new ArrayList<>();
+
+    public FlowLayout(Context context) {
+        this(context, null);
+    }
+
+    public FlowLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, -1);
+    }
+
+    public FlowLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        lines.clear();
+        maxHeights.clear();
+
+        int parentWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int maxWidth = parentWidth - PADDING * 2;
+        //当前行的所占的宽度
+        int currWidth = 0;
+        //当前行的index
+        int currLine = 0;
+        //当前行最大的高度
+        int maxHeight = 0;
+        //是否换行
+        boolean isTurn = false;
+        for (int i = 0; i < getChildCount(); i++) {
+            View childView = getChildAt(i);
+            //测量子View的大小
+            measureChild(childView, widthMeasureSpec, heightMeasureSpec);
+
+            int childWidth = childView.getMeasuredWidth();
+            int childHeight = childView.getMeasuredHeight();
+
+            int temp = currWidth + PADDING + childWidth;
+            if (temp > maxWidth) {
+                //说明当前行放不下当前子View
+                //换行
+                currWidth = childWidth;
+                currLine++;
+                isTurn = true;
+
+                //新增一行
+                addLine(childView);
+                maxHeight = childHeight;
+            } else {
+                if (i == 0) {
+                    //新增一行
+                    addLine(childView);
+
+                    maxHeight = childHeight;
+
+                    currWidth = childWidth;
+                } else {
+                    //当前行增加子View
+                    addCurrentLineChildView(childView, lines.get(currLine));
+
+                    maxHeight = Math.max(childHeight, maxHeight);
+
+                    currWidth = temp;
+                }
+
+                isTurn = false;
+            }
+
+            if (maxHeights.size() == 0 || isTurn) {
+                maxHeights.add(maxHeight);
+            } else {
+                maxHeights.set(currLine, maxHeight);
+            }
+        }
+
+        int parentHeight = PADDING * 2;
+        for (int i = 0; i < maxHeights.size(); i++) {
+            parentHeight = parentHeight + maxHeights.get(i) + LINE_INTERVAL;
+        }
+
+
+        setMeasuredDimension(parentWidth, parentHeight);
+    }
+
+    private void addCurrentLineChildView(View childView, List<View> views) {
+        views.add(childView);
+    }
+
+    private void addLine(View childView) {
+        List<View> line = new ArrayList<>();
+        line.add(childView);
+        lines.add(line);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+        int left = PADDING;
+        int top = PADDING;
+        for (int i = 0; i < lines.size(); i++) {
+            List<View> line = lines.get(i);
+            for (int j = 0; j < line.size(); j++) {
+                View childView = line.get(j);
+
+                int childMeasureWidth = childView.getMeasuredWidth();
+                childView.layout(left, top, left + childMeasureWidth, top + childView.getMeasuredHeight());
+
+                left = left + childMeasureWidth + ITEM_INVTERVAL;
+            }
+            left = PADDING;
+            top = top + maxHeights.get(i) + LINE_INTERVAL;
+        }
+    }
+
+
+    private int dp2px(int dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
+    }
+
+    private int sp2px(int sp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, getResources().getDisplayMetrics());
+    }
+}
---
+++ app/src/main/java/com/example/flowlayout/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.flowlayout;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FlowLayout flowLayout;
+
+    private String[] titles = {"Android开发艺术探索","Thinking in Java","Web","IOS","Android","不同的技术规划"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        flowLayout = findViewById(R.id.flowLayout);
+    }
+
+    public void changeFlowCount(View view){
+
+        String title = "";
+        switch (view.getId()){
+            case R.id.button0:
+                int index = new Random().nextInt(6);
+                title = titles[index];
+                break;
+            case R.id.button1:
+                title = "测试第一个测试第一个测-->试第一个测试第一个测试第一个";
+                break;
+        }
+        TextView textView = new TextView(this);
+        ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        textView.setLayoutParams(lp);
+        textView.setMaxWidth((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,200,getResources().getDisplayMetrics()));
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP,16);
+        textView.setBackgroundResource(R.drawable.bg_corner);
+
+        textView.setText(title);
+
+        flowLayout.addView(textView);
+    }
+
+    public void deleteItem(View view){
+        int childCount = flowLayout.getChildCount();
+
+        int index = 0;
+        switch (view.getId()){
+            case R.id.button2:
+                index = childCount - 1;
+                break;
+            case R.id.button3:
+                //第三个
+                index = 2;
+                break;
+        }
+        View childView = flowLayout.getChildAt(index);
+        flowLayout.removeView(childView);
+    }
+}
